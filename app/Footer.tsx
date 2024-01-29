cf602c70a6163528e54e2fe1c049c8fc6a03779c
albert <239856559@qq.com>
2024-01-29 23:25:44 +0800

    bump

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -10,28 +10,15 @@ const Link = ({ href, children }: { href: string; children: string }) => (
 
 export const Footer = ({ className }: { className?: string }) => {
   return (
-    <footer
-      className={cn(
-        "flex flex-col items-center justify-between space-y-2 py-6 text-sm text-neutral-11 sm:flex-row sm:space-y-0",
-        className
-      )}
-    >
+    <footer className={cn("flex flex-col items-center justify-between space-y-2 py-6 text-sm text-neutral-11 sm:flex-row sm:space-y-0", className)}>
       <p>
-        © 2023 AlbertM —— Built with{" "}
-        <Link href="https://beta.nextjs.org/">Next.js</Link>,{" "}
-        <Link href="https://mdxjs.com/">MDX</Link>,{" "}
-        <Link href="https://tailwindcss.com/">Tailwind</Link> and{" "}
-        <Link href="https://vercel.com/">Vercel</Link>
+        © 2023 AlbertM —— Built with <Link href="https://beta.nextjs.org/">Next.js</Link>, <Link href="https://mdxjs.com/">MDX</Link>,{" "}
+        <Link href="https://tailwindcss.com/">Tailwind</Link> and <Link href="https://vercel.com/">Vercel</Link>
       </p>
       <div className="flex space-x-4">
         {sns.map((item) => {
           const Icon = withToolTipLogoIcon(item);
-          return (
-            <Icon
-              key={item.content}
-              className="h-6 w-6 text-neutral-9 hover:text-neutral-11"
-            />
-          );
+          return <Icon key={item.content} className="h-6 w-6 text-neutral-9 hover:text-neutral-11" />;
         })}
       </div>
     </footer>
--- app/Header.tsx
+++ app/Header.tsx
@@ -4,15 +4,8 @@ import { forwardRef } from "react";
 import { cn } from "@/lib/utils";
 import { Avatar } from "@/app/Avatar";
 
-export const Header = forwardRef<
-  React.ElementRef<"header">,
-  React.ComponentPropsWithoutRef<"header">
->(({ className, ...props }, ref) => (
-  <header
-    ref={ref}
-    className={cn("flex items-center justify-between py-6", className)}
-    {...props}
-  >
+export const Header = forwardRef<React.ElementRef<"header">, React.ComponentPropsWithoutRef<"header">>(({ className, ...props }, ref) => (
+  <header ref={ref} className={cn("flex items-center justify-between py-6", className)} {...props}>
     <Avatar />
     <Navbar />
     <ThemeButton />
